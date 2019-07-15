6f7b148abf50423c2ef3f70cc5dd073a12abb27f
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2019-07-15 18:34:15 +0200

    testing: form scaffolding

---
+++ Makefile
@@ -0,0 +1,15 @@
+##
+# Generic Wielder
+#
+
+NOSE := nose2
+
+all:
+	@echo targets:
+	@echo \* test -- trigger testing
+
+.PHONY: test
+test: 
+	${NOSE} --verbose --config ./test/nose2.cfg
+
+
---
+++ test/time_engine_test.py
@@ -0,0 +1,23 @@
+
+import unittest
+import os
+import glob
+from taskw import TaskWarrior
+from logic.core import Projector
+from logic.cnfg import CnfgMediator
+import datetime as dt
+
+
+class timeTesting(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def form_test_items():
+        raise NotImplementedError("part of setUp strategy")
+
+    def test_engine(self):
+        pass
