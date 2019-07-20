117bd71b5b8ab6b091c15e8907481e6ecce92a81
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2019-07-20 10:46:32 +0200

    time_engine_test: refine for first unit test
    - case: no tasks catched

--- test/time_engine_test.py
+++ test/time_engine_test.py
@@ -2,22 +2,62 @@
 import unittest
 import os
 import glob
-from taskw import TaskWarrior
-from logic.core import Projector
-from logic.cnfg import CnfgMediator
 import datetime as dt
+from taskw import TaskWarrior
+from logic import Collector
+from logic import core
+from logic import discerner
+
+import smtpd.DebuggingServer as TestMailServer
 
 
 class timeTesting(unittest.TestCase):
 
+    def load_tw_cnfg(self):
+        return os.path.join(self.local_dir, "./cnfg/.taskrc")
+
+    def stage_tw(self, cnfg_file):
+        overrides = {'data': {'location':  os.path.join(self.local_dir,
+                     'task')}}
+        tw = TaskWarrior(config_filename=cnfg_file,
+                         config_overrides=overrides)
+        self.tw = tw
+
     def setUp(self):
-        pass
+        self.local_dir = os.path.dirname(__file__)
+        cnfg_file = self.load_tw_cnfg()
+        self.stage_tw(cnfg_file)
+
+        self.form_test_items()
+
+        self.tasks = Collector(cnfg_file)
+
+        local_dir = os.path.dirname(__file__)
+        cnfg_file = os.path.join(local_dir,
+                                 "./cnfg.yml")
+        core.ARGS.cnfg = cnfg_file
+        self.mail_server = TestMailServer(("localhost", 25),
+                                          ("localhost", 25))
 
     def tearDown(self):
-        pass
+        data_reside = os.path.join(self.local_dir, "task", "*.data")
+        for tw_data_f in glob.glob(data_reside):
+            os.remove(tw_data_f)
 
-    def form_test_items():
-        raise NotImplementedError("part of setUp strategy")
+    def form_test_items(self):
+        self.tw.task_add("TaskA", project="ProjA",
+                         scheduled="20190630T120000Z1",
+                         due="20190701T120000Z1")
+        self.tw.task_add("TaskB", project="ProjA",
+                         scheduled="20190701T120000Z1",
+                         due="20190702T120000Z1")
+        self.tw.task_add("TaskC", project="ProjB",
+                         scheduled="20190710T120000Z1",
+                         due="20190715T120000Z1")
 
-    def test_engine(self):
-        pass
+    def test_none(self):
+        discerner.NOW = dt.datetime.strptime("20190620T120000Z1",
+                                             discerner.TASK_DATE_FMT)
+        core.run()
+
+        # assert local mails
